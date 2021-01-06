5d02f601fcfe8f076005f02edd18bf9026c52e4e
Abdullah El-Menawy <a.elmenawy@outlook.com>
2021-01-06 19:58:55 +0200

    configuring ef core

---
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Data/Context/ApplicantDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Hahn.ApplicatonProcess.December2020.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.December2020.Data.Context
+{
+    public class ApplicantDbContext : DbContext
+    {
+        public ApplicantDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Applicant>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(e => e.Name).IsRequired();
+                entity.Property(e => e.FamilyName).IsRequired();
+                entity.Property(e => e.Address).IsRequired();
+                entity.Property(e => e.Age).IsRequired();
+                entity.Property(e => e.CountryOfOrigin).IsRequired();
+                entity.Property(e => e.EmailAddress).IsRequired();
+                entity.Property(e => e.Hired).IsRequired();
+
+                entity.Property(e => e.Hired).HasDefaultValue(false);
+                entity.Property(e => e.Active).HasDefaultValue(true);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasDefaultValueSql("NEWID()");
+                entity.Property(e => e.CreatedOn).ValueGeneratedOnAdd().HasDefaultValueSql("GETUTCDATE()");
+                entity.Property(e => e.ModifiedOn).ValueGeneratedOnUpdate().HasDefaultValueSql("GETUTCDATE()");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Applicant> Students { get; set; }
+    }
+}
--- Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Web/Startup.cs
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Web/Startup.cs
@@ -1,6 +1,9 @@
+using Hahn.ApplicatonProcess.December2020.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -14,8 +17,16 @@ namespace Hahn.ApplicatonProcess.December2020.Web
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicantDbContext>(options => options.UseInMemoryDatabase(Configuration["DatabaseName"]));
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

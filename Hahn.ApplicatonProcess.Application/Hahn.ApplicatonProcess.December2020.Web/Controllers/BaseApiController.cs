8b0f3d097ddc8386e51556762a55eabc1f9652e3
Abdullah El-Menawy <a.elmenawy@outlook.com>
2021-01-07 00:03:03 +0200

    Adding base api controller

---
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Web/Controllers/BaseApiController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.December2020.Web.Controllers
+{
+    public abstract class BaseApiController : ControllerBase
+    {
+        public string LocalizationLanguage
+        {
+            get
+            {
+                bool language = Request.Headers.ContainsKey("lang");
+                if (!language)
+                    return "en";
+                return Request.Headers["lang"];
+            }
+        }
+    }
+}

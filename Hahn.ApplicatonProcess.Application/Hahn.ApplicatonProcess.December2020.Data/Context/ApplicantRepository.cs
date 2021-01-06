f2784110c7f1e5510f9be5d84325f9b52b91eb89
Abdullah El-Menawy <a.elmenawy@outlook.com>
2021-01-06 20:22:49 +0200

    implementing generic repository

---
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Data/Context/ApplicantRepository.cs
@@ -0,0 +1,62 @@
+﻿using Hahn.ApplicatonProcess.December2020.Domain.Interfaces;
+using Hahn.ApplicatonProcess.December2020.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.December2020.Data.Context
+{
+    public class ApplicantRepository<T> : IRepository<T>
+        where T : class, IBaseEntity
+    {
+        private readonly ApplicantDbContext _context;
+
+        public ApplicantRepository(ApplicantDbContext context) => _context = context;
+
+        public bool Create(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            return SaveChanges();
+        }
+
+        public bool Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            return SaveChanges();
+        }
+
+        public bool Delete(Guid id)
+        {
+            var entityToDelete = _context.Set<T>().FirstOrDefault(e => e.Id == id);
+            if (entityToDelete != null)
+            {
+                _context.Set<T>().Remove(entityToDelete);
+                return SaveChanges();
+            }
+            return false;
+        }
+
+        public bool Edit(T entity)
+        {
+            var editedEntity = _context.Set<T>().FirstOrDefault(e => e.Id == entity.Id);
+            editedEntity = entity;
+            return SaveChanges();
+        }
+
+        public T GetById(Guid id)
+        {
+            var query = _context.Set<T>().AsQueryable();
+            return query.FirstOrDefault(e => e.Id == id);
+        }
+
+        public IEnumerable<T> Filter(Func<T, bool> predicate)
+        {
+            return _context.Set<T>().Where(predicate);
+        }
+
+        public bool SaveChanges() => _context.SaveChanges() > 0;
+    }
+}
--- Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Domain/Interfaces/IRepository.cs
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Domain/Interfaces/IRepository.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Hahn.ApplicatonProcess.December2020.Domain.Interfaces
 {
-    public interface IRepository
+    public interface IRepository<TEntity> where TEntity : IBaseEntity
     {
+        bool Create(TEntity entity);
+        bool Delete(TEntity entity);
+        bool Delete(Guid id);
+        bool Edit(TEntity entity);
+        TEntity GetById(Guid id);
+        IEnumerable<TEntity> Filter(Func<TEntity, bool> predicate);
+        bool SaveChanges();
     }
 }

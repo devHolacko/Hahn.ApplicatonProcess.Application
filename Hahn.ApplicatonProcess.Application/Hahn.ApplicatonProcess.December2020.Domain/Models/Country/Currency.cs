f26a9506bfd016e21322c5c069ae1e7720d6f57d
Abdullah El-Menawy <a.elmenawy@outlook.com>
2021-01-06 21:46:58 +0200

    adding country models

---
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Domain/Models/Country/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.December2020.Domain.Models.Country
+{
+    public class Currency
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+        public string symbol { get; set; }
+    }
+}
---
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Domain/Models/Country/RegionalBloc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.December2020.Domain.Models.Country
+{
+    public class RegionalBloc
+    {
+        public string acronym { get; set; }
+        public string name { get; set; }
+        public List<object> otherAcronyms { get; set; }
+        public List<string> otherNames { get; set; }
+    }
+}

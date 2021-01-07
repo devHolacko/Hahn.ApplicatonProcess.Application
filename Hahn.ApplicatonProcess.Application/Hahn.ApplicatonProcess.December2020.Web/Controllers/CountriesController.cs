fcb91a266cb6eb9ea26c6a34bbf3d8337818a007
Abdullah El-Menawy <a.elmenawy@outlook.com>
2021-01-07 14:26:25 +0200

    adding countries controller

---
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Web/Controllers/CountriesController.cs
@@ -0,0 +1,21 @@
+﻿using Hahn.ApplicatonProcess.December2020.Data.Services.CountryService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.December2020.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : BaseApiController
+    {
+        private readonly ICountryDataService _countryDataService;
+        public CountriesController(ICountryDataService countryDataService)
+        {
+            _countryDataService = countryDataService;
+        }
+    }
+}

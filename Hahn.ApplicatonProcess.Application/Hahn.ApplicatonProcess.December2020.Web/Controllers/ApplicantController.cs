831fcf76eefd9d40b5008eae0a9a539e4d1cd06b
Abdullah El-Menawy <a.elmenawy@outlook.com>
2021-01-07 12:53:17 +0200

    adding api to get applicant by id

--- Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Web/Controllers/ApplicantController.cs
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Web/Controllers/ApplicantController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Hahn.ApplicatonProcess.December2020.Data.Services.ApplicantService;
+using Hahn.ApplicatonProcess.December2020.Data.Services.CountryService;
+using Hahn.ApplicatonProcess.December2020.Domain.Models;
+using Hahn.ApplicatonProcess.December2020.Web.Validators;
+using Hahn.ApplicatonProcess.December2020.Web.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +18,24 @@ namespace Hahn.ApplicatonProcess.December2020.Web.Controllers
     [ApiController]
     public class ApplicantController : BaseApiController
     {
+        private readonly IStringLocalizer<ApplicantController> _localizer;
+        private readonly IApplicantDataService _applicantDataService;
+        private readonly ICountryDataService _countryDataService;
+        private readonly IConfiguration _configuration;
+        public ApplicantController(IStringLocalizer<ApplicantController> localizer, IApplicantDataService applicantDataService, ICountryDataService countryDataService, IConfiguration configuration)
+        {
+            _localizer = localizer;
+            _applicantDataService = applicantDataService;
+            _countryDataService = countryDataService;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Get([FromRoute(Name = "id")] int id)
+        {
+            Applicant applicant = _applicantDataService.Get(id);
+            return new OkObjectResult(applicant);
+        }
     }
 }

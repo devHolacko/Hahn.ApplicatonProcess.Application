e479898627d5aa287c22b541b585e8dbf29d32b4
Abdullah El-Menawy <a.elmenawy@outlook.com>
2021-01-07 14:38:18 +0200

    Adding api to get countries list lookup

--- Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Web/Controllers/CountriesController.cs
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Web/Controllers/CountriesController.cs
@@ -1,4 +1,5 @@
 ﻿using Hahn.ApplicatonProcess.December2020.Data.Services.CountryService;
+using Hahn.ApplicatonProcess.December2020.Web.ViewModels.Base;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -17,5 +18,16 @@ namespace Hahn.ApplicatonProcess.December2020.Web.Controllers
         {
             _countryDataService = countryDataService;
         }
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(DataGenericResponse<List<KeyValuePair<string, string>>>), 200)]
+        public async Task<IActionResult> GetCountriesLookup()
+        {
+            List<KeyValuePair<string, string>> countries = await _countryDataService.GetCountries();
+            DataGenericResponse<List<KeyValuePair<string, string>>> response = new DataGenericResponse<List<KeyValuePair<string, string>>> { Success = true, Data = countries };
+            return new OkObjectResult(response);
+        }
     }
+
 }

9e1722ef0925626ba6c5c4ee58d60bb664d33fcf
Abdullah El-Menawy <a.elmenawy@outlook.com>
2021-01-09 13:11:07 +0200

    fixing typo

--- Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Web/ViewModels/Applicant/ApplicantListItem.cs
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Web/ViewModels/Applicant/ApplicantListItem.cs
@@ -9,7 +9,7 @@ namespace Hahn.ApplicatonProcess.December2020.Web.ViewModels.Applicant
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string FullName { get; set; }
+        public string FamilyName { get; set; }
         public string Country { get; set; }
         public bool Hired { get; set; }
     }
